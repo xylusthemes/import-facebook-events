3474ee614aa6ece090973222f948409953865505
Xylus Dev <support@xylusinfo.com>
2020-04-04 14:56:44 +0530

    Added API cache for reduce API calls to Facebook.

--- includes/class-import-facebook-events-fb-authorize.php
+++ includes/class-import-facebook-events-fb-authorize.php
@@ -24,11 +24,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Import_Facebook_Events_FB_Authorize {
 
 	/**
+	 * Facebook API version
+	 *
+	 * @var string
+	 */
+	private $api_version;
+
+	/**
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
 	 */
 	public function __construct() {
+		$this->api_version = 'v6.0';
 		add_action( 'admin_post_ife_facebook_authorize_action', array( $this, 'ife_facebook_authorize_user' ) );
 		add_action( 'admin_post_ife_facebook_authorize_callback', array( $this, 'ife_facebook_authorize_user_callback' ) );
 	}
@@ -45,14 +53,13 @@ class Import_Facebook_Events_FB_Authorize {
 			$app_id            = isset( $ife_options['facebook_app_id'] ) ? $ife_options['facebook_app_id'] : '';
 			$app_secret        = isset( $ife_options['facebook_app_secret'] ) ? $ife_options['facebook_app_secret'] : '';
 			$redirect_url      = admin_url( 'admin-post.php?action=ife_facebook_authorize_callback' );
-			$api_version       = 'v3.0';
 			$param_url         = rawurlencode( $redirect_url );
 			$ife_session_state = md5( uniqid( rand(), true ) ); // phpcs:ignore WordPress.WP.AlternativeFunctions.rand_rand
 			setcookie( 'ife_session_state', $ife_session_state, '0', '/' );
 
 			if ( ! empty( $app_id ) && ! empty( $app_secret ) ) {
 
-				$dialog_url = 'https://www.facebook.com/' . $api_version . '/dialog/oauth?client_id='
+				$dialog_url = 'https://www.facebook.com/' . $this->api_version . '/dialog/oauth?client_id='
 						. $app_id . '&redirect_uri=' . $param_url . '&state='
 						. $ife_session_state . '&scope=groups_access_member_info,user_events,pages_show_list';
 				header( 'Location: ' . $dialog_url );
@@ -80,12 +87,11 @@ class Import_Facebook_Events_FB_Authorize {
 				$app_id       = isset( $ife_options['facebook_app_id'] ) ? $ife_options['facebook_app_id'] : '';
 				$app_secret   = isset( $ife_options['facebook_app_secret'] ) ? $ife_options['facebook_app_secret'] : '';
 				$redirect_url = admin_url( 'admin-post.php?action=ife_facebook_authorize_callback' );
-				$api_version  = 'v3.0';
 				$param_url    = rawurlencode( $redirect_url );
 
 			if ( ! empty( $app_id ) && ! empty( $app_secret ) ) {
 
-				$token_url = 'https://graph.facebook.com/' . $api_version . '/oauth/access_token?'
+				$token_url = 'https://graph.facebook.com/' . $this->api_version . '/oauth/access_token?'
 				. 'client_id=' . $app_id . '&redirect_uri=' . $param_url
 				. '&client_secret=' . $app_secret . '&code=' . $code;
 
@@ -100,9 +106,11 @@ class Import_Facebook_Events_FB_Authorize {
 					$access_token                               = $body_response->access_token;
 					$ife_user_token_options['authorize_status'] = 1;
 					$ife_user_token_options['access_token']     = sanitize_text_field( $access_token );
+					$token_transient_key = 'ife_facebook_access_token';
+					delete_transient( $token_transient_key );
 					update_option( 'ife_user_token_options', $ife_user_token_options );
 
-					$profile_call = wp_remote_get( 'https://graph.facebook.com/' . $api_version . "/me?fields=id,name,picture&access_token=$access_token" );
+					$profile_call = wp_remote_get( 'https://graph.facebook.com/' . $this->api_version . "/me?fields=id,name,picture&access_token=$access_token" );
 					$profile      = wp_remote_retrieve_body( $profile_call );
 					$profile      = json_decode( $profile );
 					if ( isset( $profile->id ) && isset( $profile->name ) ) {
@@ -115,7 +123,7 @@ class Import_Facebook_Events_FB_Authorize {
 					update_option( 'ife_fb_authorize_user', $ife_fb_authorize_user );
 
 					$args          = array( 'timeout' => 15 );
-					$accounts_call = wp_remote_get( 'https://graph.facebook.com/' . $api_version . "/me/accounts?access_token=$access_token&limit=100&offset=0", $args );
+					$accounts_call = wp_remote_get( 'https://graph.facebook.com/' . $this->api_version . "/me/accounts?access_token=$access_token&limit=100&offset=0", $args );
 					$accounts      = wp_remote_retrieve_body( $accounts_call );
 					$accounts      = json_decode( $accounts );
 					$accounts      = isset( $accounts->data ) ? $accounts->data : array();
