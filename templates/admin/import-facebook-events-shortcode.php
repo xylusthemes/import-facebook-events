d938c0832c9bc2035d4eea5cc5a5a0e94ed5fdb6
Xylus Dev <55250355+support-xylusthemes@users.noreply.github.com>
2021-06-11 20:29:18 +0530

    Update import-facebook-events-shortcode.php

--- templates/admin/import-facebook-events-shortcode.php
+++ templates/admin/import-facebook-events-shortcode.php
@@ -3,13 +3,13 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-$ShortcodeTable = new IFE_Shortcode_List_Table();
-$ShortcodeTable->prepare_items();
+$shortcode_table = new IFE_Shortcode_List_Table();
+$shortcode_table->prepare_items();
 
 ?>
 <div class="ife_container">
     <div class="ife_row">
     <h3 class="setting_bar"><?php esc_attr_e( 'Facebook Shortcodes', 'import-facebook-events' ); ?></h3>
-        <?php $ShortcodeTable->display(); ?>
+        <?php $shortcode_table->display(); ?>
     </div>
 </div>
