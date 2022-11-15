1e9a1623d3798ae021bfdb373f58f9a7ec10b757
Rajat Patel <prajat21@gmail.com>
2022-11-15 17:52:24 +0530

    Fixed event manager update issue

--- includes/class-import-facebook-events-em.php
+++ includes/class-import-facebook-events-em.php
@@ -370,8 +370,8 @@ class Import_Facebook_Events_EM {
 			$city    = isset( $venue['city'] ) ? $venue['city'] : '';
 			$state   = isset( $venue['state'] ) ? $venue['state'] : '';
 			$zip     = isset( $venue['zip'] ) ? $venue['zip'] : '';
-			$lat     = isset( $venue['lat'] ) ? round( $venue['lat'], 6 ) : 0.000000;
-			$lon     = isset( $venue['long'] ) ? round( $venue['long'], 6 ) : 0.000000;
+			$lat     = !empty( $venue['lat'] ) ? round( $venue['lat'], 6 ) : 0.000000;
+			$lon     = !empty( $venue['long'] ) ? round( $venue['long'], 6 ) : 0.000000;
 
 			// Save metas.
 			update_post_meta( $location_id, '_blog_id', $blog_id );
